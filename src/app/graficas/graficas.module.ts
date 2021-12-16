395f9ee3493ae34f3f2e0d27592799716bde37a6
martin0123478 <martin_agui97@outlook.es>
2021-12-15 19:09:04 -0600

    creando grafica de barras

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ChartsModule } from 'ng2-charts';
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
@@ -13,7 +13,8 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
   declarations: [BarrasComponent, BarrasDobleComponent, DonaComponent, DonaHttpComponent, GraficaBarraComponent],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    ChartsModule
   ]
 })
 export class GraficasModule { }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-barras',
@@ -7,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BarrasComponent implements OnInit {
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      xAxes:[{}],
+      yAxes:[{}]
+    },
+    plugins: {
+      legend: {
+        display: true,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end'
+      }
+    }
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartLabels: Label[] = [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ]
+
+  public barChartData: ChartDataSets[]= [
+    {data:[65,59,80,81,56,55,40], label:'serie A'},
+    {data:[28,48,40,19,86,27,90], label:'serie B'},
+  ]
+  chart: any;
+  // public barChartData: ChartData = {
+  //   labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+  //   datasets: [
+  //     { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+  //     { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+  //   ]
+  // };
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  
 
 }
+
+
